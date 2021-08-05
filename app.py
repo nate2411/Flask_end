3398c0f2ab3ae5d837feadfbf21795b083090339
Adam-smh <africa.adam1@gmail.com>
2021-08-05 15:40:56 +0200

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import hmac
 import sqlite3
-import datetime
 
 from flask_cors import CORS
-
-from flask import Flask, request, jsonify, render_template
+from datetime import timedelta
+from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
 
 
@@ -16,34 +15,32 @@ class User(object):
 
 
 def create_user_table():
-    conn = sqlite3.connect('flask_db.db')
     print("Opened database successfully")
 
     with sqlite3.connect('flask_db.db') as connection:
-        conn.execute("CREATE TABLE IF NOT EXISTS user("
-                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "first_name TEXT NOT NULL,"
-                     "last_name TEXT NOT NULL,"
-                     "username TEXT NOT NULL,"
-                     "email_address TEXT NOT NULL,"
-                     "address TEXT NOT NULL,"
-                     "password TEXT NOT NULL)")
+        connection.execute("CREATE TABLE IF NOT EXISTS user("
+                           "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                           "first_name TEXT NOT NULL,"
+                           "last_name TEXT NOT NULL,"
+                           "username TEXT NOT NULL,"
+                           "email_address TEXT NOT NULL,"
+                           "address TEXT NOT NULL,"
+                           "password TEXT NOT NULL)")
 
     print("user table created successfully")
 
 
 def create_product_table():
-    conn = sqlite3.connect('flask_db.db')
     print("Opened database successfully")
 
     with sqlite3.connect('flask_db.db') as connection:
-        conn.execute("CREATE TABLE IF NOT EXISTS product("
-                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "name TEXT NOT NULL,"
-                     "description TEXT NOT NULL,"
-                     "price TEXT NOT NULL,"
-                     "category TEXT NOT NULL,"
-                     "review TEXT NOT NULL)")
+        connection.execute("CREATE TABLE IF NOT EXISTS product("
+                           "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                           "name TEXT NOT NULL,"
+                           "description TEXT NOT NULL,"
+                           "price TEXT NOT NULL,"
+                           "category TEXT NOT NULL,"
+                           "review TEXT NOT NULL)")
 
     print("user table created successfully")
 
@@ -52,11 +49,12 @@ def fetch_users():
     with sqlite3.connect('flask_db.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user")
-        users = cursor.fetchall()
+        db_users = cursor.fetchall()
 
         new_data = []
 
-        for data in users:
+        for data in db_users:
+            print(data)
             new_data.append(User(data[0], data[3], data[6]))
     return new_data
 
@@ -79,11 +77,12 @@ users = fetch_users()
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
 
-
 app = Flask(__name__)
 
 app.debug = True
+
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=86400)
 
 CORS(app)
 
@@ -120,6 +119,32 @@ def user_registration():
         return response
 
 
+@app.route("/user-login/", methods=["POST"])
+def login():
+    response = {}
+
+    if request.method == "POST":
+
+        username = request.form['username']
+        password = request.form['password']
+
+        with sqlite3.connect("flask_db.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'")
+            user_information = cursor.fetchone()
+
+        if user_information:
+            response["user_info"] = user_information
+            response["message"] = "Success"
+            response["status_code"] = 201
+            return jsonify(response)
+
+        else:
+            response['message'] = "Login Unsuccessful, please try again"
+            response['status_code'] = 401
+            return jsonify(response)
+
+
 @app.route('/add-product/', methods=["POST"])
 @jwt_required()
 def add_product():
@@ -145,7 +170,7 @@ def add_product():
 
 
 @app.route('/show-products/', methods=["GET"])
-def get_blogs():
+def show_products():
     response = {}
 
     with sqlite3.connect("flask_db.db") as conn:
@@ -161,13 +186,13 @@ def get_blogs():
     return response
 
 
-@app.route('/view-product/<int:post_id>/', methods=["GET"])
-def get_post(post_id):
+@app.route('/view-product/<int:product_id>/', methods=["GET"])
+def view_product(product_id):
     response = {}
 
     with sqlite3.connect("flask_db.db") as conn:
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM product WHERE id={str(post_id)}")
+        cursor.execute(f"SELECT * FROM product WHERE id={str(product_id)}")
 
         response["status_code"] = 200
         response["description"] = "Product retrieved successfully"
@@ -176,49 +201,91 @@ def get_post(post_id):
     return jsonify(response)
 
 
-@app.route('/edit-post/<int:post_id>/', methods=["PUT"])
+@app.route('/edit-product/<int:product_id>/', methods=["PUT"])
 @jwt_required()
-def edit_post(post_id):
+def edit_product(product_id):
     response = {}
 
     if request.method == "PUT":
-        with sqlite3.connect('flask_db.db') as connection:
-            incoming_data = dict(request.json)
-            put_data = {}
+        print(request.json)
+        incoming_data = dict(request.json)
 
-            if incoming_data.get("title") is not None:
-                put_data["title"] = incoming_data.get("title")
+        put_data = {}
 
-                with sqlite3.connect('flask_db.db') as connection:
-                    cursor = connection.cursor()
-                    cursor.execute("UPDATE product SET title =? WHERE id=?", (put_data["title"], post_id))
-                    connection.commit()
-                    response['message'] = "Update was successfully"
-                    response['status_code'] = 200
+        print(incoming_data.get("name"))
+        if incoming_data.get("name") is not None:
+            put_data["name"] = incoming_data.get("name")
 
-            if incoming_data.get("content") is not None:
-                put_data['content'] = incoming_data.get('content')
+            with sqlite3.connect('flask_db.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"UPDATE product SET name = '{str(put_data['name'])}' WHERE id = {str(product_id)}")
+                connection.commit()
+                response['message'] = "Update was successfully"
+                response['status_code'] = 200
 
-                with sqlite3.connect('flask_db.db') as connection:
-                    cursor = connection.cursor()
-                    cursor.execute("UPDATE product SET content =? WHERE id=?", (put_data["content"], post_id))
-                    connection.commit()
+        if incoming_data.get("description") is not None:
+            put_data['description'] = incoming_data.get('description')
+            print(put_data)
 
-                    response["content"] = "Content updated successfully"
-                    response["status_code"] = 200
+            with sqlite3.connect('flask_db.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    f"UPDATE product SET description = '{str(put_data['description'])}' WHERE id = {str(product_id)}")
+                connection.commit()
+
+                response["message"] = "Content updated successfully"
+                response["status_code"] = 200
+
+        if incoming_data.get("price") is not None:
+            put_data['price'] = incoming_data.get('price')
+
+            with sqlite3.connect('flask_db.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"UPDATE product SET price = '{str(put_data['price'])}' WHERE id = {str(product_id)}")
+                connection.commit()
+
+                response["content"] = "Content updated successfully"
+                response["status_code"] = 200
+
+        if incoming_data.get("category") is not None:
+            put_data['category'] = incoming_data.get('category')
+
+            with sqlite3.connect('flask_db.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    f"UPDATE product SET category = '{str(put_data['category'])}' WHERE id = {str(product_id)}")
+                connection.commit()
+
+                response["content"] = "Content updated successfully"
+                response["status_code"] = 200
+
+        if incoming_data.get("review") is not None:
+            put_data['review'] = incoming_data.get('review')
+
+            with sqlite3.connect('flask_db.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"UPDATE product SET review = '{str(put_data['review'])}'  WHERE id = {str(product_id)}")
+                connection.commit()
+
+                response["content"] = "Content updated successfully"
+                response["status_code"] = 200
     return response
 
 
-@app.route("/delete-product/<int:post_id>", methods=["GET"])
+@app.route("/delete-product/<int:product_id>", methods=["GET"])
 @jwt_required()
-def delete_product(post_id):
+def delete_product(product_id):
     response = {}
     with sqlite3.connect("flask_db.db") as conn:
         cursor = conn.cursor()
-        cursor.execute(f"DELETE FROM product WHERE id={str(post_id)}")
+        cursor.execute(f"DELETE FROM product WHERE id={str(product_id)}")
         conn.commit()
 
         response['status_code'] = 200
         response['message'] = "Product deleted successfully."
 
     return response
+
+
+if __name__ == '__main__':
+    app.run()
